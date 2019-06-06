14e06e1447da6af4f4bb285a912f0d7bf6af31b7
Bengt Marten Agren <marangisto@me.com>
2019-06-06 19:53:25 +0800

    Use divMod and simplify.

--- src/Euclidean.hs
+++ src/Euclidean.hs
@@ -21,11 +21,10 @@ ezip (x:xs) (y:ys) = (x ++ y) : ezip xs ys
 -- Based on http://www.maths.usyd.edu.au/u/joachimw/talk2.pdf
 euclidean2' :: Int -> Int -> [Int]
 euclidean2' k n
-    | a == 0 = replicate k (n `div` k)
+    | a == 0 = replicate k b
     | otherwise = concatMap f $ euclidean2' a k 
-    where a = n `mod` k
-          f x = replicate (x - 1) (floor n_k) ++ [ ceiling n_k ]
-              where n_k = fromIntegral n / fromIntegral k
+    where (b, a) = n `divMod` k
+          f x = replicate (x - 1) b ++ [b + 1]
 
 euclidean2 :: Int -> Int -> [Bool]
 euclidean2 0 n = replicate n False
@@ -35,11 +34,9 @@ euclidean2 k n = concatMap f $ euclidean2' k n
 display :: [Bool] -> String
 display = map (\b -> if b then 'X' else '.')
 
-euclidean = euclidean2
-
 main :: IO ()
 main = do
+    mapM_ (putStrLn . display) [ euclidean2 k n | n <- [1..100], k <- [0..n] ]
     forM_ [ (1,2), (1,3), (4,12), (2,3), (2,5), (3,4), (3,5), (3,8), (7,12), (7,16) ] $ \e@(k, n) -> do
         putStrLn $ unwords [ show e, ":", display (euclidean1 k n), "    ", display (euclidean2 k n) ]
-    mapM_ (putStrLn . display) [ euclidean2 k n | n <- [1..100], k <- [0..n] ]
 
