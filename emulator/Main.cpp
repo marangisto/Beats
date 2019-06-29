5a75ed8d3aa49c38a17f04afd9c96d559ea643a3
Bengt Marten Agren <marangisto@me.com>
2019-06-29 13:22:00 +0800

    Draft GUI layout.

--- emulator/Main.cpp
+++ emulator/Main.cpp
@@ -10,6 +10,9 @@ using namespace fontlib;
 
 typedef display_t<240, 240> display;
 
+static const color_t normal_bg = web_gray;
+static const font_t& normal_font = fontlib::cmuntt_24;
+
 void text_box
     ( uint16_t      x
     , uint16_t      y
@@ -43,31 +46,99 @@ void text_box
     if (bpad)
         pen.fill_rectangle(x + lpad, y + th + tpad, tw, bpad);
 
-    //pen_t<display>(black).rectangle(x-1+lpad, y-1+tpad, tw+2, th+2);
-    pen_t<display>(black).rectangle(x-1, y-1, w+2, h+2);
+    pen_t<display>(black).rectangle(x, y, w, h);
     tr.set_pos(x + lpad, y + tpad - font.min_y);
     tr.write(s);
-    display::render();
 }
+
+struct top_bar
+{
+    static const uint16_t lw = display::width() / 3;
+    static const uint16_t rw = display::width() - lw;
+    static const uint16_t h = 30;
+
+    static constexpr uint16_t height() { return h; }
+
+    static void render()
+    {
+        text_box(0,  0, lw, h, normal_font, yellow, normal_bg, "140.0");
+        text_box(lw, 0, rw, h, normal_font, yellow, normal_bg, "1: (5, 14)");
+    }
+};
+
+struct bot_bar
+{
+    static const uint16_t lw = display::width() / 3;
+    static const uint16_t rw = lw;
+    static const uint16_t mw = display::width() - lw - rw;
+    static const uint16_t h = 30;
+    static const uint16_t y = display::height() - h;
+
+    static constexpr uint16_t height() { return h; }
+
+    static void render()
+    {
+        text_box(0,  y, lw, h, normal_font, yellow, normal_bg, "Run");
+        text_box(lw, y, mw, h, normal_font, yellow, normal_bg, "Func");
+        text_box(lw + mw, y, rw, h, normal_font, yellow, normal_bg, "Steps");
+    }
+};
+
+template<uint8_t CH>
+struct channel_graph
+{
+    static const uint16_t hpad = 6;
+    static const uint16_t w = display::width() / 8;
+    static const uint16_t h = display::height() - top_bar::height() - bot_bar::height();
+    static const uint16_t x = CH * w + hpad;
+    static const uint16_t y = top_bar::height();
+    static const uint16_t w_ = w - 2 * hpad;
+
+    static void render_step(uint8_t h_, uint8_t i, bool b)
+    {
+        if ((i + 1) * h_ >= h)
+            return;
+
+        uint8_t ha = h_ >> 1;
+        uint8_t hb = h_ - ha;
+        uint16_t y_ = y + h - (i + 1) * h_;
+
+        color_t bg = CH == 4 ? red : dark_slate_gray;
+        color_t fg = yellow;
+
+        pen_t<display>(bg).fill_rectangle(x, y_, w_, hb);
+        pen_t<display>(b ? fg : bg).fill_rectangle(x, y_ + hb, w_, ha);
+    }
+
+    static void render(uint64_t s)
+    {
+        //pen_t<display>(orange_red).fill_rectangle(x, y, w_, h);
+
+        for (uint8_t i = 0; i < 64; ++i)
+            render_step(3, i, s & (1ul << i));
+    }
+};
 
 void run()
 {
-    display::initialize(3);
-    display::clear(slate_gray);
-    pen_t<display> pen(white);
+    display::initialize("Beats 1.0", 1);
+    display::clear(black);
 
-    pen.rectangle(0, 0, display::width(), display::height());
+    top_bar::render();
+    bot_bar::render();
+    channel_graph<0>::render(0x1111ul);
+    channel_graph<1>::render(0);
+    channel_graph<2>::render(0);
+    channel_graph<3>::render(0);
+    channel_graph<4>::render(0x1111deadbeef1111ul);
+    channel_graph<5>::render(0xff);
+    channel_graph<6>::render(0);
+    channel_graph<7>::render(0);
+
     display::render();
 
-    text_box(50,  50, 100, 25, fontlib::cmunss_20, yellow, dark_red, "-");
-    text_box(50, 100, 100, 25, fontlib::cmunss_20, yellow, rebecca_purple, "Hello World!");
-    text_box(50, 150, 50, 25, fontlib::cmunss_20, yellow, teal, "j-g");
-    text_box(130, 150, 10, 15, fontlib::cmunss_20, yellow, olive_drab, "-----H j-g-----");
-    text_box(20, 200, 50, 25, fontlib::cmunss_20, yellow, lime_green, "//");
-    text_box(130, 200, 50, 25, fontlib::cmunss_20, yellow, orange_red, "l//g");
-
     bool quit = false;
-    font_t font = fontlib::cmunrm_48;
+    const font_t& font = normal_font;
     text_renderer_t<display> tr(font, white, red, true);
 
     uint16_t r = font.start_row();
