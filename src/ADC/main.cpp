d0f1fa092df6ec5db4377ae2e0ca303cf26c980b
Bengt Marten Agren <maraingist@me.com>
2020-02-23 13:05:00 +0800

    Initial ADC setup.

--- src/ADC/main.cpp
+++ src/ADC/main.cpp
@@ -1,7 +1,11 @@
 #include "../Firmware/board.h"
 #include <widget.h>
+#include <adc.h>
 
 using hal::sys_tick;
+using namespace hal::adc;
+
+typedef adc_t<1> adc;
 
 void clock_trigger() {}
 void reset_trigger() {}
@@ -11,18 +15,20 @@ int main()
     board::setup();
 
     const fontlib::font_t& font = fontlib::cmunvt_28;
-    static valuebox_t<board::tft, show_int> box1, box2, box3, box4;
+    static valuebox_t<board::tft, show_int> box0, box1, box2, box3, box4;
     static vertical_t<board::tft> col;
     static border_t<board::tft> panel;
     static color::color_t bg = color::steel_blue;
     static color::color_t fg = color::yellow;
     static bool quiet = false;
 
+    box0.setup(font, fg, bg, 0, &quiet);
     box1.setup(font, fg, bg, 0, &quiet);
     box2.setup(font, fg, bg, 0, &quiet);
     box3.setup(font, fg, bg, 0, &quiet);
     box4.setup(font, fg, bg, 0, &quiet);
     col.setup();
+    col.append(&box0);
     col.append(&box1);
     col.append(&box2);
     col.append(&box3);
@@ -32,11 +38,19 @@ int main()
     panel.layout(50, 60);
     panel.render();
 
+    adc::setup();
+    adc::sequence<14, 15, 8, 9>();
+    adc::enable();
+
     for (;;)
     {
         static int8_t i = 0;
 
-        box1 = i++;
+        box0 = i++;
+        box1 = adc::read();
+        box2 = adc::read();
+        box3 = adc::read();
+        box4 = adc::read();
         sys_tick::delay_ms(10);
     }
 }
