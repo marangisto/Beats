bbe5815c8b45efaccb58c114e4d42572d5c22d2d
Bengt Marten Agren <maraingist@me.com>
2020-02-23 14:23:02 +0800

    Working ADC DMA.

--- src/ADC/main.cpp
+++ src/ADC/main.cpp
@@ -1,11 +1,23 @@
 #include "../Firmware/board.h"
 #include <widget.h>
 #include <adc.h>
+#include <dma.h>
+#include <timer.h>
 
 using hal::sys_tick;
+using hal::sys_clock;
 using namespace hal::adc;
+using namespace hal::dma;
+using namespace hal::timer;
 
-typedef adc_t<1> adc;
+typedef hal::adc::adc_t<1> adc;
+typedef hal::dma::dma_t<1> adc_dma;
+typedef hal::timer::timer_t<15> adc_tim;
+
+static const uint8_t adc_dma_ch = 1;
+static const uint8_t adc_buf_size = 4;
+static uint16_t adc_buf[adc_buf_size];
+static const uint32_t adc_sample_freq = 2000;
 
 void clock_trigger() {}
 void reset_trigger() {}
@@ -38,19 +50,27 @@ int main()
     panel.layout(50, 60);
     panel.render();
 
+    adc_tim::setup(1, (sys_clock::freq() >> 1) / adc_sample_freq - 1);
+    adc_tim::master_mode<adc_tim::mm_update>();
+
+    adc_dma::setup();
+
     adc::setup();
-    adc::sequence<14, 15, 8, 9>();
+    adc::sequence<8, 9, 14, 15>();
+    adc::dma<adc_dma, adc_dma_ch>(adc_buf, adc_buf_size);
+    adc::trigger<0x4>();    // FIXME: use symbol for TIM15_TRIGO
     adc::enable();
+    adc::start_conversion();
 
     for (;;)
     {
         static int8_t i = 0;
 
         box0 = i++;
-        box1 = adc::read();
-        box2 = adc::read();
-        box3 = adc::read();
-        box4 = adc::read();
+        box1 = adc_buf[2];
+        box2 = adc_buf[1];
+        box3 = adc_buf[3];
+        box4 = adc_buf[0];
         sys_tick::delay_ms(10);
     }
 }
