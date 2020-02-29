37aded5e019cd80350a7c37503313281ce3e9165
Bengt Marten Agren <maraingist@me.com>
2020-02-29 18:18:08 +0800

    Started adding channel logic.

--- src/Firmware/main.cpp
+++ src/Firmware/main.cpp
@@ -4,11 +4,55 @@
 #include "clock.h"
 
 static const uint16_t trigger_pulse_length = 5;
-static const uint16_t trigger_led_length = 75;
+static const uint16_t trigger_led_length = 10;
+
+
+struct trigger_t
+{
+    void setup() { m_rate = m_skew = m_count = 0; }
+
+    bool fire(uint32_t i)
+    {
+        i += m_skew;
+
+        if (m_rate < 0)     // division via counter
+            if ((i & clock::clock_mask) == 0 && --m_count <= m_rate)
+            {
+                m_count = 0;
+                return true;
+            }
+            else
+                return false;
+        else                // power of 2 multiplier
+            return (i & (clock::clock_mask >> m_rate)) == 0;
+    }
+
+    volatile int8_t  m_rate;
+    volatile int8_t  m_skew;
+    volatile int8_t  m_count;
+};
+
+struct channel_t
+{
+    void setup()
+    {
+        m_trig.setup();
+    }
+
+    bool fire(uint32_t i)
+    {
+        return m_trig.fire(i);
+    }
+
+    trigger_t      m_trig;
+};
+
+static channel_t chan[8];
+static const uint8_t nchan = sizeof(chan) / sizeof(*chan);
 
 void clock_tick(uint32_t i)
 {
-    if ((i & clock::clock_mask) == (clock::clock_multiplier >> 1))   // pulse on centre-slot
+    if (chan[0].fire(i))
     {
         board::led0::pulse(trigger_led_length);
         board::out0::pulse(trigger_pulse_length);
@@ -25,6 +69,11 @@ int main()
     splash.show();
     gui.setup();
     gui.render();
+
+    for (uint8_t i = 0; i < nchan; ++i)
+        chan[i].setup();
+
+    chan[0].m_trig.m_rate = 0;
 
     message_t m;
 
