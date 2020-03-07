7fb70b686e8b0618a9082e027389a2128ac0cef9
Bengt Marten Agren <maraingist@me.com>
2020-03-07 15:35:05 +0800

    Use new widget.

--- src/ADC/main.cpp
+++ src/ADC/main.cpp
@@ -2,6 +2,7 @@
 #include <widget.h>
 
 using hal::sys_tick;
+using namespace color;
 
 void clock_trigger() { board::led5::toggle(); }
 void reset_trigger() { board::led5::toggle(); }
@@ -9,20 +10,18 @@ void reset_trigger() { board::led5::toggle(); }
 int main()
 {
     board::setup();
+    theme_t theme = { white, slate_gray, dim_gray, yellow, orange_red, fontlib::cmunvt_28 };
 
-    const fontlib::font_t& font = fontlib::cmunvt_28;
     static valuebox_t<board::tft, show_int> box0, box1, box2, box3, box4;
     static vertical_t<board::tft> col;
     static border_t<board::tft> panel;
-    static color::color_t bg = color::steel_blue;
-    static color::color_t fg = color::yellow;
     static bool quiet = false;
 
-    box0.setup(font, fg, bg, 0, &quiet);
-    box1.setup(font, fg, bg, 0, &quiet);
-    box2.setup(font, fg, bg, 0, &quiet);
-    box3.setup(font, fg, bg, 0, &quiet);
-    box4.setup(font, fg, bg, 0, &quiet);
+    box0.setup(theme, 0, &quiet);
+    box1.setup(theme, 0, &quiet);
+    box2.setup(theme, 0, &quiet);
+    box3.setup(theme, 0, &quiet);
+    box4.setup(theme, 0, &quiet);
     col.setup();
     col.append(&box0);
     col.append(&box1);
@@ -30,8 +29,8 @@ int main()
     col.append(&box3);
     col.append(&box4);
     panel.setup(&col, color::black);
-    panel.constrain(10, board::tft::width() - 100, 10, board::tft::height());
-    panel.layout(50, 60);
+    panel.constrain(10, board::tft::width() - 150, 10, board::tft::height());
+    panel.place(75, 60);
     panel.render();
 
     for (;;)
