963730d43e1ccfc56c0d22f03b5544f977d2baac
Bengt Marten Agren <maraingist@me.com>
2020-03-14 19:46:47 +0800

    Upgraded ADC test for new widget.

--- src/ADC/main.cpp
+++ src/ADC/main.cpp
@@ -10,25 +10,14 @@ void reset_trigger() { board::led5::toggle(); }
 int main()
 {
     board::setup();
-    theme_t theme = { white, slate_gray, dim_gray, yellow, orange_red, fontlib::cmunvt_28 };
 
-    static valuebox_t<board::tft, show_int> box0, box1, box2, box3, box4;
-    static vertical_t<board::tft> col;
-    static border_t<board::tft> panel;
-    static bool quiet = false;
+    static theme_t t = { white, slate_gray, dim_gray, yellow, orange_red, fontlib::cmunvt_28 };
+    static bool q = false;
+    static valuebox_t<board::tft, show_int> box0(t, 0, &q), box1(t, 0, &q), box2(t, 0, &q);
+    static valuebox_t<board::tft, show_int> box3(t, 0, &q), box4(t, 0, &q);
+    static vertical_t<board::tft> col(&box0, &box1, &box2, &box3, &box4);
+    static border_t<board::tft> panel(&col, t.border_color);
 
-    box0.setup(theme, 0, &quiet);
-    box1.setup(theme, 0, &quiet);
-    box2.setup(theme, 0, &quiet);
-    box3.setup(theme, 0, &quiet);
-    box4.setup(theme, 0, &quiet);
-    col.setup();
-    col.append(&box0);
-    col.append(&box1);
-    col.append(&box2);
-    col.append(&box3);
-    col.append(&box4);
-    panel.setup(&col, color::black);
     panel.constrain(10, board::tft::width() - 150, 10, board::tft::height());
     panel.place(75, 60);
     panel.render();
